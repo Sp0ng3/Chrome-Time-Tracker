dd6c56a66c5886c9e75deeeb9e36fcbb862c03df
Sp0ng3 <milanaugust@gmail.com>
2017-09-18 20:56:51 -0700

    Setting up communications
    Now the background is ready to manipulate the HTML popup dom

--- background.js
+++ background.js
@@ -2,9 +2,15 @@
 chrome.browserAction.setBadgeBackgroundColor({ color: [77, 165, 223, 0] });
 
 //NOW THE DIRTY CODE ENDS, AND THE CLEAN SPLEAN CODE IS BELOW BY THE AMAZING MILAN
+
+//Time variables
 var seconds = 0;
 var minutes = 0;
 var hours = 0;
+var timeString = "";
+
+
+
 function main(){
 	setTimeout(function(){
 		seconds += 1;
@@ -16,7 +22,18 @@ function main(){
 			minutes -= 60;
 			hours += 1;
 		}
-		chrome.browserAction.setBadgeText({text: hours.toString() + ":" + minutes.toString() + ":" + seconds.toString()}) 
+		timeString = hours.toString() + ":" + minutes.toString() + ":" + seconds.toString();
+		timeAbv = hours.toString() + ":" + minutes.toString();
+		chrome.browserAction.setBadgeText({text: timeAbv});
+		
+		
+		//Change the DOCUMENT
+		var poppy = chrome.extension.getViews({
+			type: "popup"
+		});
+		for (var i = 0; i < poppy.length; i++){
+			poppy[i].document.getElementById('time').innerHTML = timeString;
+		}
     /*
     TO-DO:  Set up the badge so it doesn't show hour or minutes when it is at zero.
     */
