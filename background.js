6800bc27c03aee90c9a9edeec09ff3b560e29d0d
Colin <colinhalebrown@gmail.com>
2017-09-18 20:08:31 -0700

    work on the Icon situation
    I added an Icons folder to hold all the shananigans that may occur

--- background.js
+++ background.js
@@ -1,4 +1,6 @@
 chrome.browserAction.setBadgeBackgroundColor({ color: [0, 0, 0, 0] });
 chrome.browserAction.setBadgeText({text: '1'});
-alert("I am running you mother lover");
+alert("I am running you mother trucker");
 chrome.browserAction.setBadgeText({text: '2'});
+alert("I am running you mother lover");
+//chrome.browserAction.setIcon(path: {"icons/icon2.png");
