7d1c689bf165079fe799d8f8e7b5d466a4cd14e2
Colin <colinhalebrown@gmail.com>
2017-09-18 19:53:14 -0700

    adds badge
    start of the second timer

--- background.js
+++ background.js
@@ -1,1 +1,4 @@
-alert("I am running you mother trucker");
+chrome.browserAction.setBadgeBackgroundColor({ color: [0, 0, 0, 0] });
+chrome.browserAction.setBadgeText({text: '1'});
+alert("I am running you mother lover");
+chrome.browserAction.setBadgeText({text: '2'});
