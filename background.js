dbb7214cb2e462b607d1e528acc36bccaa701ece
Sp0ng3 <milanaugust@gmail.com>
2017-09-19 20:43:44 -0700

    Save Time (Not Working)
    Variables are glitching out pretty hard.

--- background.js
+++ background.js
@@ -1,7 +1,4 @@
-
 chrome.browserAction.setBadgeBackgroundColor({ color: [77, 165, 223, 0] });
-
-//NOW THE DIRTY CODE ENDS, AND THE CLEAN SPLEAN CODE IS BELOW BY THE AMAZING MILAN
 
 //Time variables
 var seconds = 0;
@@ -10,10 +7,31 @@ var hours = 0;
 var timeString = "";
 var timeAbv = "";
 
+var saveSecond = 0;
+var saveMinute = 0;
+var saveHour = 0;
 
 var LEGACYHOURS = 0;
 var LEGACYMINUTES = 0;
 var LEGACYSECONDS = 0;
+
+if(localStorage.getItem("UHOURS") != null){
+	 LEGACYHOURS = localStorage.getItem("UHOURS");
+}
+if(localStorage.getItem("UMINUTES") != null){
+	LEGACYMINUTES = localStorage.getItem("UMINUTES");
+	if(LEGACYMINUTES > 60){
+		LEGACYMINUTES = 0;
+		LEGACYMINUTES += 1;
+	}
+}
+if(localStorage.getItem("USECONDS") != null){
+	LEGACYSECONDS = localStorage.getItem("USECONDS");
+	if(LEGACYSECONDS > 60){
+		LEGACYSECONDS = 0;
+		LEGACYMINUTES += 1;
+	}
+}
 
 var ALLhours = 0;
 var ALLminutes = 0;
@@ -21,8 +39,7 @@ var ALLseconds = 0;
 
 var timeTotal = "";
 
-var SWITCH = false;
-var RETRIVED = false;
+
 
 function main(){
 	setTimeout(function(){
@@ -40,39 +57,40 @@ function main(){
 		timeAbv = hours.toString() + ":" + minutes.toString();
 		chrome.browserAction.setBadgeText({text: timeAbv});
 		
-		if (SWITCH == true && RETRIVED == false){
-			chrome.storage.local.get('UHOURS', function(result){
-				//console.log("hours gotten");
-				//console.log(result.UHOURS);
-				LEGACYHOURS = result.UHOURS;
-				
-			});
-			chrome.storage.local.get('UMINUTES', function(result){
-				//console.log("minutes gotten");
-				//console.log(result.UMINUTES);
-				LEGACYMINUTES = result.UMINUTES;
-			});
-			chrome.storage.local.get('USECONDS', function(result){
-				//console.log("seconds gotten");
-				//console.log(result.USECONDS);
-				LEGACYSECONDS = result.USECONDS - 1;
-			});
-			RETRIVED = true;
-			
-			
-			
-		}
+		
+		
+		
 		ALLhours = LEGACYHOURS + hours;
-		ALLminutes = LEGACYMINUTES + minutes;
+		
+		
 		ALLseconds = LEGACYSECONDS + seconds;
-		timeTotal = ALLhours.toString() + ":" + ALLminutes.toString() + ":" + ALLseconds.toString();
-		//Save total time
+		if(ALLseconds > 60){
+			ALLseconds = 0;
+			ALLminutes += 1;
+		}
 		
-		chrome.storage.local.set({'UHOURS':ALLhours, 'UMINUTES': ALLminutes, "USECONDS": ALLseconds}, function(){
-			//console.log("TIME SAVED");
-		});
+		ALLminutes = LEGACYMINUTES + minutes;
+		if(ALLminutes > 60){
+			ALLminutes = 0;
+			ALLhours += 1;
+		}
 		
 		
+		timeTotal = LEGACYHOURS.toString() + ":" + LEGACYMINUTES.toString() + ":" + LEGACYSECONDS.toString();		
+		
+		//SAVE TIME
+		saveSecond = LEGACYSECONDS + seconds;
+		if(saveSecond > 60){
+			saveSecond = 0;
+			saveMinute += 1;
+		}
+		if(saveMinute > 60){
+			saveMinute = 0;
+			saveHour += 1;
+		}
+		localStorage.setItem("UHOURS", LEGACYHOURS + hours);
+		localStorage.setItem("UMINUTES", LEGACYMINUTES + minutes);
+		localStorage.setItem("USECONDS", LEGACYSECONDS + seconds);
 		
 		//Change the DOCUMENT
 		var poppy = chrome.extension.getViews({
@@ -83,7 +101,6 @@ function main(){
 			poppy[i].document.getElementById('atime').innerHTML = timeTotal;
 		}
 		
-		SWITCH = true;
     /*
     TO-DO:  Set up the badge so it doesn't show hour or minutes when it is at zero.
     */
